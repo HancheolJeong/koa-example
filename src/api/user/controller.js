f1790c8265f3655306d490144c34625ed42dbbe5
= <juyz0730@gmail.com>
2023-01-17 14:58:34 +0900

    암호화 추가

--- src/api/user/controller.js
+++ src/api/user/controller.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const {register} = require('./query');
+const crypto = require('crypto');
 
 exports.info = (ctx, next) => {
     let id = ctx.params.id;
@@ -6,13 +8,42 @@ exports.info = (ctx, next) => {
 }
 
 exports.register = async (ctx, next) => {
-    let token = await generteToken({name : 'my-name'});
-    ctx.body = token;
+    // let token = await generteToken({name : 'my-name'});
+    // ctx.body = token;
+    let {email, password, name} = ctx.request.body;
+    let result = crypto.pbkdf2Sync(password, process.env.APP_KEY, 50, 255, 'sha512')
+
+    let {affectedRows} = await register(email, result.toString('base64'), name);
+
+    if(affectedRows> 0)
+    {
+        let token = await gererteToken({name});
+        ctx.body = token;
+    }
+    else
+    {
+        ctx.body = {result:"fail"};
+    }
 }
 exports.login = async (ctx, next) => {
 
-    let token = await generteToken({name: 'my-name'});
-    ctx.body = token;
+    // let token = await generteToken({name: 'my-name'});
+    // ctx.body = token;
+    let {email, password} = ctx.request.body;
+    let result = crypto.pbkdf2Sync(password, process.env.APP_KEY, 50, 255, 'sha512')
+
+    let item = await this.login(email, result.toString('base64'));
+
+    if(item == null)
+    {
+        ctx.body = {result: "fail"};
+    }
+    else
+    {
+        let token = await generteToken({name: item.name});
+        ctx.body = token;
+    }
+
 }
 
 let generteToken = (payload)=>{
